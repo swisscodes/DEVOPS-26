a1e3a043d77c289e009b299b753089e38dd80007
Obafemi Lawal <swissbobo@gmail.com>
2026-07-30 10:32:44 +0200

    added submodule finally

---
+++ Python/automate-the-boring-stuff/chapter1-1.py
@@ -0,0 +1,23 @@
+print("Hello, World!")
+
+print("My name is " + input("enter your name: ") + " and I am learning Python.")
+
+# This is a comment. It is ignored by the interpreter.
+"""This is a multi-line comment.
+It can span multiple lines."""
+
+for i in range(5):
+    print("This is line number " + str(i + 1))
+
+for i in range(5):
+    print("This is line number " + str(i + 1))
+
+
+count = 0
+while count < 3:
+    print("This is a finite loop (will run 3 times).")
+    count += 1
+
+def ok() {
+    print
+}
